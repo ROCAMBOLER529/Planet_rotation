327359863960183d0ad63cece344225564cd6298
ROCAMBOLER529 <42423816+ROCAMBOLER529@users.noreply.github.com>
2023-04-01 19:22:02 -0300

    Update Planeta.cs

--- Planeta.cs
+++ Planeta.cs
@@ -1,30 +1,47 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Planeta : MonoBehaviour
 {
-    public float velocidad;
-    public float rotacion;
-    private Sprite imagen;
+    private float velocidad = 30f;
+    private int vueltas = 0;
+    // private Vector3 vector;
     
-    // Start is called before the first frame update - crearPlaneta()
+    // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(Vector3.up * velocidad * Time.deltaTime);
+
+        if (transform.localRotation.eulerAngles.y > 359.0f) {
+            vueltas++;
+        }
+
     }
 
-    void acelerar() {
-
+    public void Ralentizar(float ralen) {
+        velocidad = velocidad - ralen;
     }
 
-    void ralentizar() {
-        
+    public float getVelocidad() {
+        return velocidad;
+    }
+
+    public void AumentarVelocidad(float aceleracion) {
+        velocidad = velocidad + aceleracion;
+    }
+
+    public void setVelocidad(float vel) {
+        velocidad = vel;
+    }
+
+    public int getVueltas() {
+        return vueltas;
     }
 }
