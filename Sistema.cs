f7c2da1ded6929fa85d0453b56f8fa4064422cee
ROCAMBOLER529 <42423816+ROCAMBOLER529@users.noreply.github.com>
2023-04-01 19:21:37 -0300

    Update Sistema.cs

--- Sistema.cs
+++ Sistema.cs
@@ -1,69 +1,108 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Rotar3 : MonoBehaviour
+public class Sistema : MonoBehaviour
 {    
-    private boolean pausa;
-    private int tapss;
-    private int time = [];
-    
-    // Pasar a planeta
-    private int vueltas;
-    
-    public GameObject planeta;
-    public GameObject interface;
+    private float timer = 0f;
+    private float ralen = 1f;
+    private Planeta planetaScript;
+    private SpriteRenderer vacioSprite;
+    private bool pausa = false;
+
+    public Sprite sprite; // referencia al sprite de Game Over
+    public GameObject vacio; // el Objeto de fondo
+    public GameObject planeta; // el Objeto planeta
+    public Text contador; // el texto que cuenta el tiempo
+    public Text contVueltas; // el texto que cuenta las vueltas
     
     // Start is called before the first frame update - arrancarJurgo()
     void Start()
     {
-        
+        planetaScript = planeta.GetComponent<Planeta>();
+        vacioSprite = vacio.GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {       
+        if (getVel() == 0f) {
+            contador.text = "You Lost";
+            vacioSprite.sprite = sprite;
+            contVueltas.text = "Max. de vueltas: " + planetaScript.getVueltas();
+        } else {
+            timer += Time.deltaTime;
+            contador.text = "" + timer.ToString();
+            sumarVueltas();
+
+            if (timer >= ralen) {
+                if (getVel() <= 15f) {
+                    planetaScript.setVelocidad(0f);
+                }
+
+                planetaScript.Ralentizar(getVel() * 0.3f);
+                Debug.Log("Ralentizar planeta");
+                timer = 0f;
+            }
+
+            if (Input.GetKeyDown("space")) {
+                planetaScript.AumentarVelocidad(5f);
+                Debug.Log("AUMENTAR");
+            }
+        }  
+    }
+
+    private float getVel() {
+        return planetaScript.getVelocidad();
+    }
+
+    // void mostrarInstrucciones() {
+
+    // }
+
+    // public void pausar() {
+    //     if (pausa) {
+    //         Time.timeScale = 1f;
+            
+    //         pausa = false;
+    //     } else {
+    //         Time.timeScale = 0f;
+
+    //         pausa = true;
+    //     }
+    // }
+
+    // void despausar() {
+    //     pausa = false;
+    // }
+
+    // GameObject getMusica() {
+    //     // return musica
+    // }
+
+    // void checkearSave() {
+
+    // }
+
+    // void autoSve() {
+
+    // }
+
+    void sumarVueltas() {
+        contVueltas.text = "Vueltas: " + planetaScript.getVueltas();
+    }
+
+    // void maxVueltas() {
+
+    // }
+
+    // void sumarTaps(ticks) {
         
-    }
+    // }
 
-    void mostrarInstrucciones() {
-
-    }
-
-    void pausar() {
-        pausa = true;
-    }
-
-    void despausar() {
-        pausa = false;
-    }
-
-    float getVelocidad() {
-        return planeta.velocidad;
-    }
-
-    GameObject getMusica() {
-        // return musica
-    }
-
-    void checkearSave() {
-
-    }
-
-    void autoSve() {
-
-    }
-
-    int contVueltas(vueltas) {
-        return vueltas;
-    }
-
-    void maxVueltas() {
-
-    }
-
-    int contTaps(ticks) {
-        return ticks++;
-    }
+    // void cambiarDimension() {
+    //
+    // }
 
 }
