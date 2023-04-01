a953902b62d5af13cd73ffcf24ce52ebb60b2c4d
ROCAMBOLER529 <42423816+ROCAMBOLER529@users.noreply.github.com>
2023-04-01 19:22:27 -0300

    Update Interface.cs

--- Interface.cs
+++ Interface.cs
@@ -1,34 +1,34 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
 
-public class Interface : MonoBehaviour
-{
-    // Aca vana  aestar las musicas
-    public GameObject tracks = [];
+// public class Interface : MonoBehaviour
+// {
+//     // Aca vana  aestar las musicas
+//     public GameObject tracks = [];
 
-    // Aca va a estar los paneles, pueden haber mas
-    public GameObject imagen = [];
+//     // Aca va a estar los paneles, pueden haber mas
+//     public GameObject imagen = [];
     
-    // Start is called before the first frame update - crearInterface()
-    void Start()
-    {
+//     // Start is called before the first frame update - crearInterface()
+//     void Start()
+//     {
         
-    }
+//     }
 
-    // Update is called once per frame
-    void Update()
-    {
+//     // Update is called once per frame
+//     void Update()
+//     {
         
-    }
+//     }
 
-    void mostrarMenu() {
+//     void mostrarMenu() {
 
-    }
+//     }
 
-    void cambiarMusica() {
+//     void cambiarMusica() {
 
-    }
+//     }
 
     
-}
+// }
